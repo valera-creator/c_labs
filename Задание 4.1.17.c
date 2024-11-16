107155fd12bfcdbd1495285768c69cc582fd6059
valera <149261827+valera-creator@users.noreply.github.com>
2024-11-16 13:44:55 +0300

    Update Задание 4.1.17.c

--- Задание 4.1.17.c
+++ Задание 4.1.17.c
@@ -8,6 +8,32 @@ struct list
 	struct list* next;
 };
 
+
+int insert_end(int val, struct list** head, struct list** last)
+{
+	// функция вернет 1, если нет ошибок с выделением памяти, иначе 0, если есть
+	struct list* cur;
+	cur = (struct list*)malloc(sizeof(struct list));
+
+	if (cur == NULL) // если ошибка выделения памяти
+		return 0;
+
+	cur->next = NULL;
+	cur->inf = val;
+	if ((*head) == NULL)
+	{
+		(*head) = cur;
+		(*last) = cur;
+	}
+	else
+	{
+		(*last)->next = cur;
+		(*last) = cur;
+	}
+	return 1;
+}
+
+
 int input_list(struct list** head, struct list** last)
 {
 	int res, val;
@@ -38,31 +64,6 @@ int input_list(struct list** head, struct list** last)
 }
 
 
-int insert_end(int val, struct list** head, struct list** last)
-{
-	// функция вернет 1, если нет ошибок с выделением памяти, иначе 0, если есть
-	struct list* cur;
-	cur = (struct list*)malloc(sizeof(struct list));
-
-	if (cur == NULL) // если ошибка выделения памяти
-		return 0;
-
-	cur->next = NULL;
-	cur->inf = val;
-	if ((*head) == NULL)
-	{
-		(*head) = cur;
-		(*last) = cur;
-	}
-	else
-	{
-		(*last)->next = cur;
-		(*last) = cur;
-	}
-	return 1;
-}
-
-
 void print_list(struct list** head)
 {
 	struct list* cur = (*head);
@@ -85,20 +86,19 @@ void print_list(struct list** head)
 
 int write_elems_new_list(struct list** head_first, struct list** head_second, struct list** last_second)
 {
-	int i;
 	struct list* cur_first_list = (*head_first);
 	struct list* prev_first_list = (*head_first);
 	int cur_num_elems = 0;
-	
+
 	while (cur_first_list != NULL)
-	{	
-		cur_num_elems += 1;
+	{
+		cur_num_elems ++;
 		if (cur_first_list->inf > cur_num_elems)
 		{
 			if (cur_first_list == (*head_first)) // если самый первый элемент из первого списка подошел
-			{	
+			{
 				if ((*head_second) == NULL) // если во втором списке еще ничего нет
-				{	
+				{
 					(*head_second) = (*head_first);
 					(*last_second) = (*head_first);
 					(*head_first) = (*head_first)->next;
@@ -107,7 +107,7 @@ int write_elems_new_list(struct list** head_first, struct list** head_second, st
 					(*head_second)->next = NULL;
 				}
 				else // если во втором списке уже что-то есть (на прошлом этапе из первого списка забрали элемент, а текущий элемент снова подошел)
-				{	
+				{
 					(*last_second)->next = (*head_first);
 					(*last_second) = (*head_first);
 					(*head_first) = (*head_first)->next;
@@ -115,12 +115,12 @@ int write_elems_new_list(struct list** head_first, struct list** head_second, st
 					cur_first_list = cur_first_list->next;
 					(*last_second)->next = NULL;
 				}
-				
+
 			}
 			else
 			{
 				if ((*head_second) == NULL) // создаем первый элемент второго списка
-				{	
+				{
 					(*head_second) = cur_first_list;
 					(*last_second) = cur_first_list;
 					prev_first_list->next = cur_first_list->next;
@@ -128,7 +128,7 @@ int write_elems_new_list(struct list** head_first, struct list** head_second, st
 					(*head_second)->next = NULL;
 				}
 				else // добавляем уже к существующему элементу во второй список
-				{	
+				{
 					(*last_second)->next = cur_first_list;
 					(*last_second) = cur_first_list;
 					prev_first_list->next = cur_first_list->next;
@@ -138,10 +138,10 @@ int write_elems_new_list(struct list** head_first, struct list** head_second, st
 			}
 		}
 		else
-		{	
+		{
 			prev_first_list = cur_first_list;
 			cur_first_list = cur_first_list->next;
-			
+
 		}
 	}
 }
@@ -164,10 +164,10 @@ void clear_list(struct list** head, struct list** last)
 int main()
 {
 	setlocale(0, "");
-	struct list* head_first, * last_first, *head_second, *last_second;
+	struct list* head_first, * last_first, * head_second, * last_second;
 	head_first = last_first = head_second = last_second = NULL;
-	
-	
+
+
 	if (!input_list(&head_first, &last_first)) // ошибка выделения памяти возникла
 	{
 		return 0;
@@ -175,10 +175,15 @@ int main()
 
 	printf("\nПервый список до изменений: ");
 	print_list(&head_first);
-	write_elems_new_list(&head_first, &head_second, &last_second);
-	printf("\n\nПервый список после изменений: ");
-	print_list(&head_first);
-	printf("\n\nВторой список: ");
+
+	if (head_first != NULL)
+	{
+		write_elems_new_list(&head_first, &head_second, &last_second);
+		printf("\nПервый список после изменений: ");
+		print_list(&head_first);
+	}
+	
+	printf("\nВторой список: ");
 	print_list(&head_second);
 
 	clear_list(&head_first, &last_first);
