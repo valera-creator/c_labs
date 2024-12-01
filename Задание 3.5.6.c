85fbf26d74f35e472a0df17f0ea801bf1bd072c2
valera <149261827+valera-creator@users.noreply.github.com>
2024-12-01 17:10:20 +0300

    Create Задание 3.5.6.c

---
+++ Задание 3.5.6.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <locale.h>
+#define ALL_WORDS 30 
+#define ALL_BOOKS 20 + 1 // берем еще 1 символ на '\0'
+
+int input_str(int* cnt_words, char str[][ALL_BOOKS], char *end_symbol)
+{
+    int index_i = 0, index_j = 0;
+    char buf;
+
+    scanf_s("%c", &buf);
+    while (1)
+    {
+        while (('a' <= buf && buf <= 'z') || ('A' <= buf && buf <= 'Z'))
+        {   
+            // проверка на превышение букв в слове
+            if (index_j >= ALL_BOOKS - 1) 
+            {
+                printf("Ошибка: Число букв превысило допустимое значение %d", ALL_BOOKS - 1);
+                return 0;
+            }
+
+            str[index_i][index_j] = buf;
+            index_j++;
+
+            scanf_s("%c", &buf);
+        }
+
+        // Если не буква
+        if (index_j > 0) 
+        {
+            str[index_i][index_j] = '\0';
+            index_j = 0;
+            (*cnt_words)++;
+            index_i++;
+        }
+
+        // проверка на превышение слов
+        if ((*cnt_words) >= ALL_WORDS)
+        {
+            printf("Ошибка: Кол-во слов превышает допустимое значение, %d", ALL_WORDS);
+            return 0;
+        }
+
+        // Пропускаем пробелы
+        while (buf == ' ')
+        {   
+            scanf_s("%c", &buf);
+        }
+
+        // Конец строки
+        if (buf == '.' || buf == '!' || buf == '?')
+        {   
+            (*end_symbol) = buf;
+            return;
+        }
+
+        // если введено некорректно
+        if (!(('a' <= buf && buf <= 'z') || ('A' <= buf && buf <= 'Z')) && !(buf == ' ') && !((buf == '.' || buf == '!' || buf == '?')))
+        {
+            if (!(('a' <= buf && buf <= 'z') || ('A' <= buf && buf <= 'Z')))
+                printf("Была введена не латинская буква, не символ из конца предложения и не пробел\n");
+            else
+                printf("Предложение не завершилась символом из предложенных: '?', '!', '.'");
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void print_str(int cnt_words, const char str[][ALL_BOOKS], char end_symbol)
+{
+    int i;
+    printf("Введенный текст с преобразованием удаления лишних пробелов между словами: ");
+    for (i = 0; i < cnt_words; i++)
+    {   
+        if (i == cnt_words - 1)
+            printf("%s", str[i]);
+        else
+            printf("%s ", str[i]);
+    }
+    printf("%c\n", end_symbol);
+}
+
+
+int main()
+{
+    char str[ALL_WORDS][ALL_BOOKS];
+    char end_symbol;
+    int cnt_words = 0;
+    setlocale(0, "");
+
+    if (!input_str(&cnt_words, str, &end_symbol))
+        return 0;
+
+    print_str(cnt_words, str, end_symbol);
+    return 0;
+}
